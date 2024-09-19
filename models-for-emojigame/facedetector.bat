6be4a22f643869681e0ae3c27723fa03cd99a597
Erik <43537587+ErikHeggeli@users.noreply.github.com>
2024-09-19 12:47:54 +0200

    Add files via upload

---
+++ models-for-emojigame/facedetector.bat
@@ -0,0 +1,7 @@
+@echo off
+title Script to run the Vitensenter demos: drawing demo
+@echo
+start C:\Users\vitensenter_demo\Downloads\buttery-taskbar.exe
+start C:\Users\vitensenter_demo\Desktop\bats\start_face.bat
+timeout /t 200
+start C:\Users\vitensenter_demo\Desktop\bats\start_face_unreal.bat
